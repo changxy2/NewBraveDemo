2f1429191c9f86a1355359c892a3e4ce9337a47d
changxy2 <changxy2@yahoo.com.cn>
2010-11-28 19:05:07 +0800

    增加一些忽略的文件

---
+++ Solution/game/autogen/ClassIDs.h
@@ -0,0 +1,26 @@
+//
+// This is a generated file.  You should not alter this file manually.
+
+//Source enum: ClassIDs.enum
+
+#pragma once
+#ifndef EE_ClassIDs_h_INCLUDED
+#define EE_ClassIDs_h_INCLUDED
+
+#include <efd/UniversalTypes.h>
+#include <efd/ClassInfo.h>
+
+namespace game
+{
+
+    static const efd::ClassID CLASS_ID_INVALID             =          0u; // 0x00000000
+
+    static const efd::ClassID CLASS_ID_AIModel             =  102760448u; // 0x06200000
+    static const efd::ClassID CLASS_ID_Camera_base         =  102760449u; // 0x06200001
+    static const efd::ClassID CLASS_ID_Camera_smoothFollow =  102760450u; // 0x06200002
+    static const efd::ClassID CLASS_ID_MovableObject       =  102760451u; // 0x06200003
+    static const efd::ClassID CLASS_ID_MovementService     =  102760452u; // 0x06200004
+
+} // end namespace game
+
+#endif // EE_ClassIDs_h_INCLUDED
